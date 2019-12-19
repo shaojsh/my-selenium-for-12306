fd526bac7458df2c622f52af87347f320d8b03bd
junshuai shao <shaojunshuai2019@sina.com>
2019-12-19 08:03:55 +0800

    vip for movie and songs

--- movie/song.py
+++ movie/song.py
@@ -1,16 +1,23 @@
+# encoding:utf-8
 import requests
 import re
+from bs4 import BeautifulSoup
 
-api = 'http://music.taihe.com/artist'
+api = 'http://music.taihe.com/search'
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+}
 
 data = {
-    'key': '薛之谦'
+    'key': '演员'
 }
 
 # data/param区别？
-req = requests.get(api, data=data)
-html = req.text
-
+req = requests.get(api, params=data, headers=header)
+req.encoding = req.apparent_encoding
+# print(html)
 # 获取歌曲id，解析网页内容
-sids = re.findall(r'data-playdata="(.+?)"', html)
-print(sids)
+soup = BeautifulSoup(req.text, 'html.parser')
+print(soup.prettify())
+print(soup.find_all('a'))
